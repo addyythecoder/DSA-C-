917dc5b8e94b2dabd5306cdac2f1adeed6298a55
Aditya Giri <giriaditya9422@gmail.com>
2025-02-19 20:21:21 +0530

    updated files

---
+++ PR.3 Circlet/Q.5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        cout << i << " ";
+    }
+
+    for (int i = 5; i >= 1; i--) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
+
